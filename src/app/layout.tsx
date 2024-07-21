02fa68289d7601d7d133a1ecf78029b74cfe8ec6
BatraAayush <67547222+BatraAayush@users.noreply.github.com>
2024-07-21 15:10:15 +0530

    ignore ts errors for build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
-import { TRPCReactProvider } from "~/trpc/react";
-
 export const metadata: Metadata = {
   title: "Ecommerce App",
   description: "Buy anything you need",
@@ -17,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        {children}
       </body>
     </html>
   );
